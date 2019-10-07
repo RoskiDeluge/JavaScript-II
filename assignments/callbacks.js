5b8adc2350a489f6dc61e1ea9634aa27000a619c
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-10-06 17:15:21 -1000

    Working on contains cb function

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,27 +38,46 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+const test1 = getLength(items, item => item);
+console.log(test1);
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop());
 }
+
+const test2 = last(items, item => item);
+console.log(test2);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+const test3 = sumNums(10, 10, item => item);
+console.log(test3);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+const test4 = multiplyNums(2, 3, item => item);
+console.log(test4);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
 }
+
+const test5 = contains('Gum', items, item => item);
+console.log(test5);
 
 /* STRETCH PROBLEM */
 
